499c6bef12e92987deaf5798f690d097b1d2a299
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-04-13 16:59:29 +0900

    Initial Commit

---
+++ FourtyTwoCM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FourtyTwoCM
+//
+//  Created by hwijinjeong on 4/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
