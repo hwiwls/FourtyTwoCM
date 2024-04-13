afefbe3e933f033ed4e96cb7cb3e93e58116ff97
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-04-13 17:54:47 +0900

    Setting: UIColor+CustomColor 추가

---
+++ FourtyTwoCM/Extension/UIColor+CustomColor.swift
@@ -0,0 +1,21 @@
+//
+//  UIColor+CustomColor.swift
+//  FourtyTwoCM
+//
+//  Created by hwijinjeong on 4/13/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static let customColor = CustomColors()
+    
+    struct CustomColors {
+        let offWhiteColor = UIColor(named: "OffWhiteColor") ?? .white
+        
+        let placeHolderGray = UIColor(named: "PlaceHolderGray") ?? .white
+        
+        let superDarkGray = UIColor(named: "SuperDarkGray") ?? .white
+
+    }
+}
