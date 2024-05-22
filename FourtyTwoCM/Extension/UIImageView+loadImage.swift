65ee775101e0a6b574071609cea790fe2761ad0c
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-05-22 22:14:54 +0900

    [FTC-90] 에러 처리 개선 및 transform 함수 구조 개선

--- FourtyTwoCM/Extension/UIImageView+loadImage.swift
+++ FourtyTwoCM/Extension/UIImageView+loadImage.swift
@@ -8,31 +8,6 @@
 import UIKit
 import Kingfisher
 
-//extension UIImageView {
-//    func loadImage(from url: URL, placeHolderImage: UIImage? = nil) {
-//        var accessToken: String = ""
-//        
-//        do {
-//            accessToken = try Keychain.shared.getToken(kind: .accessToken)
-//        } catch {
-//            print("loadImage에서 액세스토큰을 불러오지 못함: \(error)")
-//        }
-//        
-//        guard let sesacKey = Bundle.main.sesacKey else {
-//            print("sesacKey를 로드하지 못했습니다.")
-//            return 
-//        }
-//        
-//        let modifier = AnyModifier { request in
-//            var request = request
-//            request.setValue(accessToken, forHTTPHeaderField: HTTPHeader.authorization.rawValue)
-//            request.setValue(sesacKey, forHTTPHeaderField: HTTPHeader.sesacKey.rawValue)
-//            return request
-//        }
-//        self.kf.setImage(with: url, placeholder: placeHolderImage, options: [.requestModifier(modifier), .forceRefresh])
-//    }
-//}
-
 extension UIImageView {
     func loadImage(from url: URL, placeHolderImage: UIImage? = nil, completion: (() -> Void)? = nil) {
         var accessToken: String = ""
