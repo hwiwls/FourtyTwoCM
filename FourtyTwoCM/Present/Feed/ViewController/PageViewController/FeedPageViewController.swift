f84229ba1dd126d518e70f597055dc411d87c949
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-05-29 03:11:44 +0900

    [FTC-95] 게시글 조회 로직 및 페이지네이션 로직 변경

--- FourtyTwoCM/Present/Feed/ViewController/PageViewController/FeedPageViewController.swift
+++ FourtyTwoCM/Present/Feed/ViewController/PageViewController/FeedPageViewController.swift
@@ -4,10 +4,11 @@
 //
 //  Created by hwijinjeong on 4/15/24.
 //
+
 import UIKit
 import RxSwift
 import RxCocoa
-import CoreLocation
+import Toast
 
 final class FeedPageViewController: UIPageViewController {
 
@@ -15,39 +16,25 @@ final class FeedPageViewController: UIPageViewController {
     private let viewModel = FeedPageViewModel()
     private let disposeBag = DisposeBag()
     
-    private var currentIndex: Int = 0
-    private var timer: Timer?
-    private let progressBarMaxValue: Float = 7.0
-    private var elapsedTime: Float = 0.0
-    
-    private var isLastPageReached = false
-    
-    private let locationManager = CLLocationManager()
+    private var currentIndex: Int = 0 // 현재 페이지 관리
+    private var timer: Timer? // 프로그레스 바와 페이지 전환 제어
+    private let progressBarMaxValue: Float = 7.0 // 7초. 프로그레스 바의 최대 값
+    private var elapsedTime: Float = 0.0 // 경과 시간
+    private var isLastPageReached = false // 마지막 페이지에 도달했는지
+    private var isLoadingNextPage = false // 다음 페이지 로드 중인지
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.dataSource = self
         self.delegate = self
-        requestLocationAuthorization()
         bind()
         setupTimer()
-    }
-
-    private var newPostAddedSubject = PublishSubject<Void>()
-    
-    @objc private func newPostReceived() {
-        newPostAddedSubject.onNext(())
-    }
-    
-    private func requestLocationAuthorization() {
-        Permissions.shared.checkUserDeviceLocationServiceAuthorization()
     }
     
     private func bind() {
         let trigger = Observable.just(())
-        let fetchNextPage = Observable<Void>.never()
-
-        let input = FeedPageViewModel.Input(trigger: trigger, fetchNextPage: fetchNextPage, newPostAdded: newPostAddedSubject.asObservable())
+        
+        let input = FeedPageViewModel.Input(trigger: trigger, fetchNextPage: Observable.never())
         let output = viewModel.transform(input: input)
 
         output.posts
@@ -56,28 +43,15 @@ final class FeedPageViewController: UIPageViewController {
             })
             .disposed(by: disposeBag)
         
-        Permissions.shared.isLocationAuthorized
-            .subscribe(onNext: { [weak self] isAuthorized in
-                if isAuthorized {
-                    self?.printUserLocation()
-                }
+        output.errorMessage
+            .drive(onNext: { [weak self] message in
+                self?.view.makeToast(message, duration: 2.0, position: .center)
             })
             .disposed(by: disposeBag)
     }
-    
-    private func printUserLocation() {
-        guard let currentLocation = locationManager.location else {
-            print("Failed to get current location.")
-            return
-        }
+}
 
-        viewModel.setCurrentLocation(currentLocation)
-        let latitude = currentLocation.coordinate.latitude
-        let longitude = currentLocation.coordinate.longitude
-        print("유저의 위치 - Latitude: \(latitude), Longitude: \(longitude)")
-    }
-
-    
+extension FeedPageViewController {
     private func setupViewControllers(posts: [Post]) {
         self.contentViewControllers = posts.map { post in
             let vc = FeedContentViewController()
@@ -95,28 +69,17 @@ final class FeedPageViewController: UIPageViewController {
         }
     }
     
-    private func moveToNextPage() {
-        contentViewControllers.remove(at: currentIndex) // 현재 페이지 삭제
-        
-        currentIndex = currentIndex % contentViewControllers.count // 새 인덱스 계산
-        let nextViewController = contentViewControllers[currentIndex]
-        setViewControllers([nextViewController], direction: .forward, animated: true) { [weak self] completed in
-            if completed {
-                self?.resetTimerAndProgress() // 페이지 전환 완료 후 타이머 리셋
-            }
-        }
-    }
-
     private func resetTimerAndProgress() {
         timer?.invalidate()  // 기존 타이머 중지
         elapsedTime = 0      // 경과 시간 리셋
+
+        guard currentIndex < contentViewControllers.count else { return } // Index 범위 초과를 방지
+        
         isLastPageReached = (currentIndex == contentViewControllers.count - 1) // 마지막 페이지 여부 설정
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
         updateProgressBarForCurrentPage()  // 프로그레스 바 업데이트
     }
 
-
-    
     private func setupTimer() {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
     }
@@ -131,6 +94,7 @@ final class FeedPageViewController: UIPageViewController {
             } else {
                 timer?.invalidate()
                 contentViewControllers[currentIndex].updateProgressBar(progress: 1.0) // 마지막 페이지에서는 프로그레스 바를 꽉 채우고 멈춤
+                loadNextPage() // 페이지의 마지막 게시글에 도달하면 다음 페이지 로드
             }
         } else {
             if currentIndex < contentViewControllers.count {
@@ -139,19 +103,86 @@ final class FeedPageViewController: UIPageViewController {
         }
     }
 
+    private func moveToNextPage() {
+        if currentIndex >= contentViewControllers.count - 1 {
+            loadNextPage() // 마지막 페이지에서 다음 페이지 로드
+            return
+        }
+
+        currentIndex += 1
+        let nextViewController = contentViewControllers[currentIndex]
+        setViewControllers([nextViewController], direction: .forward, animated: true) { [weak self] completed in
+            if completed {
+                self?.resetTimerAndProgress() // 페이지 전환 완료 후 타이머 리셋
+            }
+        }
+    }
+
+    private func loadNextPage() {
+        guard !isLoadingNextPage else {
+            print("다음 페이지를 로드 중입니다.")
+            return
+        }
+
+        isLoadingNextPage = true // 다음 페이지 로드 중으로 설정
+
+        let fetchNextPage = Observable.just(())
+        let input = FeedPageViewModel.Input(trigger: Observable.never(), fetchNextPage: fetchNextPage)
+        let output = viewModel.transform(input: input)
+
+        output.posts
+            .asObservable()
+            .subscribe(onNext: { [weak self] newPosts in
+                guard let self = self else { return }
+                self.addNewViewControllers(newPosts: newPosts)
+                self.isLoadingNextPage = false // 로드 완료 후 플래그 해제
+            }, onError: { [weak self] (error: Error) in
+                self?.isLoadingNextPage = false // 에러 발생 시 플래그 해제
+                print("Error loading next page: \(error.localizedDescription)")
+            }, onCompleted: { [weak self] in
+                self?.isLoadingNextPage = false // 완료 시 플래그 해제
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func addNewViewControllers(newPosts: [Post]) {
+        let newViewControllers = newPosts.map { post -> FeedContentViewController in
+            let vc = FeedContentViewController()
+            vc.loadPost(post: post)
+            vc.viewModel.postDeleteSuccess
+                .subscribe(onNext: { [weak self] _ in
+                    self?.moveToNextPage()
+                })
+                .disposed(by: vc.disposeBag)
+            return vc
+        }
+
+        let previousCount = contentViewControllers.count
+        contentViewControllers.append(contentsOf: newViewControllers)
+
+        // 새 게시글이 추가된 경우 타이머를 재설정하고 다음 페이지로 이동
+        if previousCount == contentViewControllers.count - newViewControllers.count && !newViewControllers.isEmpty {
+            currentIndex = previousCount // 추가된 첫 게시글로 인덱스 설정
+            setViewControllers([contentViewControllers[currentIndex]], direction: .forward, animated: true, completion: nil)
+            resetTimerAndProgress()
+        }
+    }
+
     
     private func resetTimer() {
         timer?.invalidate() // 기존 타이머를 중지
         elapsedTime = 0 // 경과 시간을 0으로 리셋
+        guard currentIndex < contentViewControllers.count else { return } // Index 범위 초과를 방지
         contentViewControllers[currentIndex].updateProgressBar(progress: 0) // 프로그레스 바를 초기화
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
     }
     
     private func updateProgressBarForCurrentPage() {
+        guard currentIndex < contentViewControllers.count else { return } // Index 범위 초과를 방지
         contentViewControllers[currentIndex].updateProgressBar(progress: 0)
     }
-    
 }
+
 
 extension FeedPageViewController: UIPageViewControllerDataSource {
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
@@ -194,40 +225,5 @@ extension FeedPageViewController: UIPageViewControllerDelegate {
                 }
             }
         }
-    }
-    
-    private func loadNextPage() {
-        guard let nextCursor = viewModel.next_cursor, nextCursor != "0" else {
-            print("더 이상 게시글이 없습니다.")
-            return
-        }
-        
-        let fetchNextPage = Observable.just(())
-        let input = FeedPageViewModel.Input(trigger: Observable.never(), fetchNextPage: fetchNextPage, newPostAdded: newPostAddedSubject)
-        let output = viewModel.transform(input: input)
-
-        output.posts
-            .drive(onNext: { [weak self] newPosts in
-                guard let self = self else { return }
-                self.addNewViewControllers(newPosts: newPosts)
-            })
-            .disposed(by: disposeBag)
-    }
-
-
-    private func addNewViewControllers(newPosts: [Post]) {
-        print("Adding new posts: \(newPosts.map { $0.postID })")
-        let newViewControllers = newPosts.map { post -> FeedContentViewController in
-            let vc = FeedContentViewController()
-            vc.loadPost(post: post)
-            vc.viewModel.postDeleteSuccess
-                .subscribe(onNext: { [weak self] _ in
-                    self?.moveToNextPage()
-                })
-                .disposed(by: vc.disposeBag)
-            return vc
-        }
-
-        contentViewControllers.append(contentsOf: newViewControllers)
     }
 }
