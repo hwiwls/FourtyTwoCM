7799c654238a55644ba95e013c319ece8559e9c4
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-05-31 12:05:03 +0900

    [FTC-100] PostMap와 Chatting 폴더 및 파일 추가, [FTC-123] PostMap 및 Chatting 화면 탭바에 추가

---
+++ FourtyTwoCM/Present/PostMap/ViewController/PostMapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PostMapViewController.swift
+//  FourtyTwoCM
+//
+//  Created by hwijinjeong on 6/20/24.
+//
+
+import UIKit
+
+class PostMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+
+
+}
---
+++ FourtyTwoCM/Present/PostMap/ViewModel/PostMapViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PostMapViewModel.swift
+//  FourtyTwoCM
+//
+//  Created by hwijinjeong on 6/20/24.
+//
+
+import Foundation
