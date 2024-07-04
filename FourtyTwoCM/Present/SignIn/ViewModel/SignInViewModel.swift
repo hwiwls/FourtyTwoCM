11b8ad94057fb8fca5a1b672588572e84dbb4e7f
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-07-04 16:46:22 +0900

    [FTC-143] SignInViewModel 리팩토링

--- FourtyTwoCM/Present/SignIn/ViewModel/SignInViewModel.swift
+++ FourtyTwoCM/Present/SignIn/ViewModel/SignInViewModel.swift
@@ -9,8 +9,8 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-final class SignInViewModel {
-
+final class SignInViewModel: ViewModelType {
+    
     var disposeBag = DisposeBag()
 
     struct Input {
@@ -28,53 +28,60 @@ final class SignInViewModel {
     }
 
     func transform(input: Input) -> Output {
+        
+        let isLoginButtonEnabled = BehaviorRelay(value: false)
+        let signInSuccessTrigger = PublishRelay<Void>()
+        let signInFailureRelay = PublishRelay<String>()
+        
         let emailAndPassword = Observable.combineLatest(input.emailText, input.passwordText)
             .map { SignInQuery(email: $0, password: $1) }
 
-        let isLoginButtonEnabled = emailAndPassword
+        emailAndPassword
             .map { !$0.email.isEmpty && !$0.password.isEmpty }
-            .asDriver(onErrorJustReturn: false)
-
-        let signInResult = input.signInButtonTapped
+            .bind(to: isLoginButtonEnabled)
+            .disposed(by: disposeBag)
+        
+        input.signInButtonTapped
+            .debounce(.seconds(1), scheduler: MainScheduler.instance)
             .withLatestFrom(emailAndPassword)
             .flatMapLatest { query in
                 NetworkManager.performRequest(route: Router.login(query: query), dataType: SignInModel.self)
                     .asObservable()
                     .materialize()
             }
-            .share()
-
-        let signInSuccess = signInResult
-            .compactMap { $0.element }
-            .do(onNext: { signInModel in
-                // 토큰 저장 로직
-                do {
-                    try Keychain.shared.saveToken(kind: .accessToken, token: signInModel.accessToken)
-                    try Keychain.shared.saveToken(kind: .refreshToken, token: signInModel.refreshToken)
-                    UserDefaults.standard.set(signInModel.user_id, forKey: "userID")
-                } catch {
-                    print("토큰 저장 실패: \(error)")
+            .subscribe(onNext: { event in
+                switch event {
+                case .next(let signInModel):
+                    // 토큰 저장 로직
+                    do {
+                        try Keychain.shared.saveToken(kind: .accessToken, token: signInModel.accessToken)
+                        try Keychain.shared.saveToken(kind: .refreshToken, token: signInModel.refreshToken)
+                        UserDefaults.standard.set(signInModel.user_id, forKey: "userID")
+                        signInSuccessTrigger.accept(())
+                    } catch {
+                        print("토큰 저장 실패: \(error)")
+                        signInFailureRelay.accept("토큰 저장 실패")
+                    }
+                case .error(let error):
+                    if let apiError = error as? APIError {
+                        signInFailureRelay.accept(apiError.errorMessage)
+                    } else {
+                        signInFailureRelay.accept("알 수 없는 오류가 발생했습니다.")
+                    }
+                default:
+                    break
                 }
             })
-            .map { _ in Void() }
-            .asDriver(onErrorDriveWith: .empty())
-
-        let signInFailure = signInResult
-            .compactMap { $0.error }
-            .map { error in
-                switch error {
-                case APIError.unauthorized:
-                    return "계정 혹은 비밀번호를 확인해주세요"
-                default:
-                    return "로그인 오류가 발생했습니다"
-                }
-            }
-            .asDriver(onErrorJustReturn: "알 수 없는 에러가 발생했습니다.")
+            .disposed(by: disposeBag)
         
         let signUpTrigger = input.signUpButtonTapped
             .asDriver(onErrorJustReturn: ())
 
-        return Output(isLoginButtonEnabled: isLoginButtonEnabled, signInSuccess: signInSuccess, signInFailure: signInFailure, signUpTrigger: signUpTrigger)
+        return Output(
+            isLoginButtonEnabled: isLoginButtonEnabled.asDriver(),
+            signInSuccess: signInSuccessTrigger.asDriver(onErrorJustReturn: ()),
+            signInFailure: signInFailureRelay.asDriver(onErrorJustReturn: "알 수 없는 오류가 발생했습니다."),
+            signUpTrigger: signUpTrigger
+        )
     }
 }
-
